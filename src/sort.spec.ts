f2bd06fe4a93b809d4c25ccd180f6974cb1d4817
Robert Lönnqvist <robert.lonnqvist@gmail.com>
2021-05-09 20:45:37 +0200

    Use build dir and add some more tests

--- src/sort.spec.ts
+++ src/sort.spec.ts
@@ -10,10 +10,34 @@ export default (
       expect(sorter([3, 1, 4, 2])).to.eql([1, 2, 3, 4]);
     });
 
-    it("should should not modify input", () => {
-      const input = [3, 1, 4, 2];
-      sorter(input);
-      expect(input).to.eql([3, 1, 4, 2]);
+    it("should sort short array", () => {
+      expect(sorter([3, 1])).to.eql([1, 3]);
+    });
+
+    it("should sort random array", () => {
+      const array = sorter(
+        Array(100)
+          .fill(0)
+          .map(() => Math.floor(Math.random() * 1000))
+      );
+
+      for (let i = 1; i < array.length; i++) {
+        expect(array[i]).greaterThanOrEqual(array[i - 1]);
+      }
+    });
+
+    it("should not modify input", () => {
+      const array = [3, 1, 4, 2];
+      sorter(array);
+      expect(array).to.eql([3, 1, 4, 2]);
+    });
+
+    it("should handle empty array", () => {
+      expect([]).to.eql([]);
+    });
+
+    it("should handle one element array", () => {
+      expect([1]).to.eql([1]);
     });
   });
 };
